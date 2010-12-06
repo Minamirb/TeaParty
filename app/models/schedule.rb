cb41964a80098ce271d8350b3f00ea3610d7db82
murase_syuka <murase.syuka+github@gmail.com>
2010-12-07 00:44:12 +0900

    validates schedule

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,5 +1,7 @@
 class Schedule < ActiveRecord::Base
   belongs_to :shop
   has_many :diaries, :class_name => 'Diary'
-#  has_one :shop 
+  #  has_one :shop
+  validates :title, :presence=>true
+  validates :shop_id, :presence=>true
 end
